a1b78303090d1acd9641ba99892c682f62cf6f9a
ShakeelAhmad3 <buneeri020@gmail.com>
2023-09-22 22:42:01 +0500

    conversion

---
+++ java.js
@@ -0,0 +1,9 @@
+//  let sting = false
+//  console.log(typeof sting)
+//  NumberSting = Number(sting)
+//  console.log(typeof NumberSting)
+//  console.log(NumberSting)
+
+let logIn = undefined
+logPrint = Boolean(logIn)
+console.log(logPrint)
